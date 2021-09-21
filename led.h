96316a51be8f12c12c414aa8f0d16d2dbf9ad823
Abdelrhman Dawoud <73134994+abdwd@users.noreply.github.com>
2021-09-21 17:38:51 +0200

    All files are added

---
+++ led.h
@@ -0,0 +1,20 @@
+#ifndef LED_H_
+#define LED_H_
+
+#include "dio.h"
+
+typedef struct
+{
+	EN_port port;
+	uint8_t pin;
+
+}ST_LED_config_t;
+
+
+void LED_init(ST_LED_config_t* config);
+void LED_on(ST_LED_config_t* led);
+void LED_off(ST_LED_config_t* led);
+
+
+
+#endif /* LED_H_ */
---
+++ micro_config.h
@@ -0,0 +1,22 @@
+ /******************************************************************************
+ *
+ * Module: Micro - Configuration
+ *
+ * File Name: Micro_Config.h
+ *
+ * Description: File include all Microcontroller libraries
+ *
+ * Author: Abdelrhman Dawoud
+ *
+ *******************************************************************************/
+
+#ifndef MICRO_CONFIG_H_
+#define MICRO_CONFIG_H_
+#define __AVR_ATmega32__
+
+//#include "registers.h"
+//#include <avr/interrupt.h>
+//#include <avr/io.h>
+#include <util/delay.h>
+
+#endif /* MICRO_CONFIG_H_ */
